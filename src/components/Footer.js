edcd901a2d2352ad9405fb9ed47df9d3324d0504
htmlk <79633378+htmlk-first@users.noreply.github.com>
2022-07-26 16:50:57 +0900

    footer.js 코드정리

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,40 +4,30 @@ export function Footer() {
 			<div className="footer bg-gray">
 				<div className="container">
 					<div className="row">
-						<div className="col-lg-12"></div>
-					</div>
-				</div>
-			</div>
-			<div className="copyright bg-gray">
-				<div className="container">
-					<div className="row">
 						<div className="col-lg-12">
-							<p className="p-small">
-								Copyright ©{" "}
-								<a className="no-line" href="#your-link">
-									KWIX
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
 
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-12">
-							<p className="p-small">
-								Distributed By{" "}
-								<a
-									className="no-line"
-									href="https://themewagon.com/"
-								>
-									Themewagon
-								</a>
-							</p>
 						</div>
 					</div>
 				</div>
 			</div>
+
+			<div className="copyright bg-gray">
+        		<div className="container">
+            		<div className="row">
+                		<div className="col-lg-12">
+                    		<p className="p-small">Copyright © <a className="no-line" href="#your-link">KWIX</a></p>
+                		</div> 
+            		</div> 
+        		</div>
+
+        		<div className="container">
+            		<div className="row">
+                		<div className="col-lg-12">
+                    		<p className="p-small">Distributed By <a className="no-line" href="https://themewagon.com/">Themewagon</a></p>
+                		</div> 
+            		</div> 
+        		</div>
+    		</div>
 		</div>
 	);
 }
